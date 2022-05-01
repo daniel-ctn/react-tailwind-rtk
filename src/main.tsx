4cbe140e2e52da7010829f6129d8eb62d437d9e5
Daniel Nguyen <54087410+danielnguyen5201@users.noreply.github.com>
2022-05-02 02:07:58 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { store } from 'app/store'
 import './index.css'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
