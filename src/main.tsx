1ede3e8e29c803e3bc75516ec49ab48c62e57545
Nguyen Cao Thang <thangnc2@dgroup.co>
2022-05-01 21:22:43 +0700

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import { store } from 'app/store'
 import './index.css'
 import App from './App'
 
-const container = document.getElementById('app')
-const root = createRoot(container)
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
